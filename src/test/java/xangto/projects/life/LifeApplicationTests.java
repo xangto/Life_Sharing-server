afe28354bcf329d66b98a3b345aaad188d28c005
wangtao <2281659624@qq.com>
2026-08-28 15:35:53 +0800

    feat: redis操作

--- src/test/java/xangto/projects/life/LifeApplicationTests.java
+++ src/test/java/xangto/projects/life/LifeApplicationTests.java
@@ -1,13 +1,26 @@
 package xangto.projects.life;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class LifeApplicationTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void redisTest() {
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        opsForValue.set("test", "test");
+        String string = opsForValue.get("test");
+        System.out.println(string);
     }
 
 }
